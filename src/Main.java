ab07e47910464b9e10fc7b283afba1e94fce39be
Khadija Channel <tonemail@example.com>
2026-04-04 03:03:02 +0200

    PUSH

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -7,14 +8,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(
-                FXMLLoader.load(getClass().getResource("/gestion/view/etudiant.fxml"))
-        ));
+        // Charger le FXML dans un Parent
+        Parent root = FXMLLoader.load(getClass().getResource("/gestion/view/View.fxml"));
+
+        // Créer la scène avec ce root
+        Scene scene = new Scene(root, 800, 600);
+
+        // Ajouter le CSS
+        scene.getStylesheets().add(
+                getClass().getResource("/gestion/view/Style.css").toExternalForm()
+        );
+
+        // Configurer la scène sur le stage
+        stage.setScene(scene);
         stage.setTitle("Gestion des étudiants");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
