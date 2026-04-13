e7cd9a6d8b75b776659a1a86b4d2d191bbbb7a58
Mbery diouf <mamandiouf01@gmail.com>
2026-04-13 22:08:22 +0300

    Amélioration gestion accueil

--- src/gestion/controller/accueilcontroller.java
+++ src/gestion/controller/accueilcontroller.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 
-public class AccueilController {
+public class accueilcontroller {
 
     @FXML
     private Button goEtudiants, goMatieres, goNotes, goResultats;
@@ -25,19 +25,19 @@ public class AccueilController {
         changerScene("/gestion/view/Matieres.fxml");
     }
 
-    // Méthode pour aller à la fenêtre Notes
+
     @FXML
     private void goNotes() {
         changerScene("/gestion/view/Notes.fxml");
     }
 
-    // Méthode pour aller à la fenêtre Résultats
+
     @FXML
     private void goResultats() {
         changerScene("/gestion/view/Resultats.fxml");
     }
 
-    // Fonction utilitaire pour changer de scène
+
     private void changerScene(String fxml) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
